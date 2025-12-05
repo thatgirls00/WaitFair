3049bbd280f0734c18c833ddca6599f6491ad4b2
gksdud1109 <vectorh532@gmail.com>
2025-12-05 13:59:24 +0900

    refactor: baseEntity ID 생성전략 제거

--- backend/src/main/java/com/back/global/entity/BaseEntity.java
+++ backend/src/main/java/com/back/global/entity/BaseEntity.java
@@ -8,11 +8,8 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.SequenceGenerator;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,12 +22,6 @@ import lombok.Setter;
 public abstract class BaseEntity {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
-	@SequenceGenerator(
-		name = "global_seq",          // JPA에서 부를 이름
-		sequenceName = "global_seq",  // 실제 PostgreSQL에 생성될 시퀀스 이름
-		allocationSize = 50           // 미리 땡겨올 ID 개수 -> 성능
-	)
 	@Setter(AccessLevel.PROTECTED)
 	private Long id;
 
