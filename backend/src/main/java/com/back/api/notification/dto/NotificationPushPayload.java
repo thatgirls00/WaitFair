f96ec3c2039a0e68248b764f97c6aa290cba0bd2
woongcheol Na <32201345@dankook.ac.kr>
2025-12-10 03:23:28 +0900

    local 브랜치 기능 분리 : 개발중이던 다른 도메인 파일 제거

--- backend/src/main/java/com/back/api/notification/dto/NotificationPushPayload.java
+++
@@ -1,8 +0,0 @@
-package com.back.api.notification.dto;
-
-public record NotificationPushPayload(
-	String type,                       // "NEW_NOTIFICATION"
-	long unreadCount,
-	NotificationResponseDto notification
-) { }
-
--- backend/src/main/java/com/back/api/notification/dto/UnreadCountResponseDto.java
+++
@@ -1,5 +0,0 @@
-package com.back.api.notification.dto;
-
-public record UnreadCountResponseDto(// 읽지 않은 알림 개수 응답용 DTO
-	long unreadCount
-) { }
