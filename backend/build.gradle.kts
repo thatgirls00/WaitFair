fc85b6c6067c3a64aa152f7d941f64b9246ec04e
gksdud1109 <vectorh532@gmail.com>
2025-12-09 18:10:10 +0900

    remove: 불필요 의존성 삭제

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -30,7 +30,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.reflections:reflections:0.10.2")
 
     implementation("org.springframework.boot:spring-boot-starter-batch")
     testImplementation("org.springframework.batch:spring-batch-test")
