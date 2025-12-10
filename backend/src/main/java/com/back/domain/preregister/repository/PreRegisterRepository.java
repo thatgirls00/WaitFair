4d9111f5291d08c16400a22bae24ddf7de2fed5b
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-12-10 22:26:10 +0900

    feat: 충돌 해결

--- backend/src/main/java/com/back/domain/preregister/repository/PreRegisterRepository.java
+++ backend/src/main/java/com/back/domain/preregister/repository/PreRegisterRepository.java
@@ -1,10 +1,7 @@
 package com.back.domain.preregister.repository;
 
-<<<<<<< HEAD
 import java.util.List;
-=======
 import java.util.Optional;
->>>>>>> ffb5874117a85203a12dffae47d8d57c60289e34
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
