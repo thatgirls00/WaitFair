4a32e064487a70147933ac1ea6a7272c4899afca
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-12-16 12:57:05 +0900

    feat: paymentClient 임시 @Component 처리

--- backend/src/main/java/com/back/api/payment/payment/client/PaymentClient.java
+++ backend/src/main/java/com/back/api/payment/payment/client/PaymentClient.java
@@ -1,8 +1,11 @@
 package com.back.api.payment.payment.client;
 
+import org.springframework.stereotype.Component;
+
 import com.back.api.payment.payment.dto.request.PaymentConfirmCommand;
 import com.back.api.payment.payment.dto.response.PaymentConfirmResult;
 
+@Component
 public interface PaymentClient {
 	PaymentConfirmResult confirm(PaymentConfirmCommand command);
 }
