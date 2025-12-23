c71ffbc516e7f57f6fbaf2db1356a75bfb8767fa
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-12-23 15:17:27 +0900

    feat: 22 port open

--- infra/main.tf
+++ infra/main.tf
@@ -158,6 +158,14 @@ resource "aws_security_group" "sg_1" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     to_port   = 0
